de56de742e09d1a88c46606c1da03508eff26028
dunjajdukovic <dunja.ajdukovic@yahoo.com>
2017-08-08 19:42:09 +0200

    initial SurveyController

---
+++ src/main/resources/static/js/controllers/surveyController.js
@@ -0,0 +1,10 @@
+(function() {
+	angular.module('app')
+		.controller('SurveyController', SurveyController);
+	
+	SurveyController.$inject = [SurveyService];
+	
+	function SurveyController(SurveyService) {
+		
+	};	
+});()
