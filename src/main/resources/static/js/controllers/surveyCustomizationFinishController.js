6ac50d40611c91a3fdd14bc8e46c522def03ab63
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-08-16 10:47:32 +0200

    Bootstrap optimization

--- src/main/resources/static/js/controllers/surveyCustomizationFinishController.js
+++ src/main/resources/static/js/controllers/surveyCustomizationFinishController.js
@@ -1,11 +1,40 @@
 (function(){
-	angular.module('app')
-		.controller('SurveyCustomizationFinishController', SurveyCustomizationFinishController);
+  angular.module('app')
+    .controller('SurveyCustomizationFinishController', SurveyCustomizationFinishController);
 
-	SurveyCustomizationFinishController.$inject = ['SurveyService'];
+  SurveyCustomizationFinishController.$inject = ['SurveyService', '$location', '$routeParams'];
 
-	function SurveyCustomizationFinishController(SurveyService) {
+  function SurveyCustomizationFinishController(SurveyService, $location, $routeParams) {
 
+    var self = this;
+    self.getCurrentSurvey = getCurrentSurvey;
+    self.saveSurvey = saveSurvey;
 
-	};
+    init();
+
+    function init() {
+      self.surveyHashedId = $routeParams.hashedId;
+      getCurrentSurvey();
+    }
+
+    function getCurrentSurvey() {
+      SurveyService.getCurrentSurvey(self.surveyHashedId)
+        .then(
+        function(response){
+          self.survey = response;
+        });
+    }
+
+    function saveSurvey() {
+      SurveyService.saveSurvey(self.survey)
+        .then(
+        function(response){
+          $location.path('/survey/details/' + response.hashedId);
+        }, 
+        function(error){
+          console.log(error);
+        })
+    }
+
+  };
 })();
