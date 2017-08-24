5ec0d39d82014baa7d35216e8b58c372b2710c7f
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-08-25 00:58:56 +0200

    Updated scripts.js

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,4 @@
+// FACEBOOK
 window.fbAsyncInit = function() {
 	FB.init({
 		appId            : '121554091828177',
@@ -16,6 +17,7 @@ window.fbAsyncInit = function() {
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+// TWITTER
 window.twttr = (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0],
 			t = window.twttr || {};
@@ -33,18 +35,21 @@ window.twttr = (function(d, s, id) {
 	return t;
 }(document, "script", "twitter-wjs"));
 
+// GOOGLE
 (function() {
 	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
 	po.src = 'https://apis.google.com/js/platform.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 
+// WIDGET LOAD
 loadWidgets = function() {
 	FB.XFBML.parse();
 	twttr.widgets.load();
 	gapi.plus.go();
 }
 
+// BODY SHOW
 function showBody() {
 	$('body').show();
 };
