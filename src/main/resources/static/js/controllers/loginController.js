0ed0e2767b5c0726df1fb17bc34f7f01fcf44a04
backfire96 <igor.kuzmanovic.996@gmail.com>
2017-08-25 10:11:27 +0200

    Home page syling

--- src/main/resources/static/js/controllers/loginController.js
+++ src/main/resources/static/js/controllers/loginController.js
@@ -8,6 +8,7 @@
 
     var self = this;
     self.getCredentials = getCredentials;
+    self.checkForm = checkForm;
 
     function getCredentials(credentials) {
       UserService.login(credentials).then(handleSuccessCredentials);
@@ -17,6 +18,28 @@
       self.user = data;
       $scope.mc.init();
       $location.path('/home');
+    }
+
+    function checkForm() {
+      var focusedElement;
+
+      if($scope.surveyForm.$invalid) {
+        if($scope.surveyForm.description.$invalid) {
+          $scope.surveyForm.description.$setDirty();
+          focusedElement = '#description';
+        }
+
+        if($scope.surveyForm.name.$invalid) {
+          $scope.surveyForm.name.$setDirty();
+          focusedElement = '#name';
+        }
+
+        $(focusedElement).focus();
+
+        return false;
+      }
+
+      return true;
     }
 
   };
