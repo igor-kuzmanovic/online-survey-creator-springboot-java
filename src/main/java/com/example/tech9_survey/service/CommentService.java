d074c52fe2a6572c0b3829888aac7a93e86bb631
Sale95 <dobrilovic47@gmail.com>
2017-09-07 11:22:05 +0200

    service fix

--- src/main/java/com/example/tech9_survey/service/CommentService.java
+++ src/main/java/com/example/tech9_survey/service/CommentService.java
@@ -2,16 +2,18 @@ package com.example.tech9_survey.service;
 
 import java.util.List;
 import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.tech9_survey.domain.Comment;
 import com.example.tech9_survey.repository.CommentRepository;
 
-@Transactional
 @Service
 public class CommentService {
 
 	private CommentRepository commentRepository;
 
+	@Autowired
 	public CommentService(CommentRepository commentRepository) {
 		this.commentRepository = commentRepository;
 	}
@@ -24,14 +26,17 @@ public class CommentService {
 		return commentRepository.findOne(id);
 	}
 
+	@Transactional
 	public Comment save(Comment comment) {
 		return commentRepository.save(comment);
 	}
 
+	@Transactional
 	public void delete(Long id) {
 		commentRepository.delete(id);
 	}
 
+	@Transactional
 	public void deleteByUser(String user) {
 		commentRepository.deleteByPoster(user);
 	}
